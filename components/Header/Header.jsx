520cb2b6bbc4622a7e35669d8bd7d7caf9d0cfc5
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-03-06 22:45:10 +0100

    feat: :sparkles: Progress on header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="navbar flex items-center">
+        {/* <Image src="/assets/images/logo.svg" alt="Disney+ Brand Logo" /> */}
+        <a className="cta">Suscríbete ya</a>
+        <a className="cta-secondary mr-10 inline-flex h-10">Iniciar sesión</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
+import Header from '../components/Header/Header';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
