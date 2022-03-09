c0d698cadf951af67a7dd66220e5e8129165ad7c
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-03-09 21:18:18 +0100

    feat: :sparkles: Add Footer component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import DisneyLogo from '../DisneyLogo/DisneyLogo';
+
+const Footer = () => {
+  return (
+    <footer className="my-8">
+      <div className="m-auto flex flex-col items-center space-y-2 text-xs text-silver">
+        <div className="h-20 w-20">
+          <DisneyLogo />
+        </div>
+        <div className="space-x-8">
+          <a>Política de privacidad</a>
+          <a>Política de cookies</a>
+        </div>
+        <div className="space-x-8">
+          <a>Ayuda</a>
+          <a>¿Quiénes somos?</a>
+        </div>
+        <p className="pt-4 text-center">
+          © 2021 Disney y entidades relacionadas. Reservados todos los derechos.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
