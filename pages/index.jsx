3ec3ea7cdf28d1e4a35774637212cba105664252
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-03-09 21:18:33 +0100

    feat: :sparkles: Consume new components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import bgSmall from '../public/assets/images/bg-small.jpeg';
-import bgLarge from '../public/assets/images/bg-large.jpeg';
+
+import Footer from '../components/Footer/Footer';
+import DevicesSection from '../components/DevicesSection/DevicesSection';
+import HeroSection from '../components/HeroSection/HeroSection';
+import HomeBackground from '../components/HomeBackground/HomeBackground';
 
 const Home = () => {
   return (
@@ -14,43 +16,10 @@ const Home = () => {
       </Head>
 
       <main>
-        <div className="absolute top-0 -z-10 h-full w-full md:hidden">
-          <Image
-            alt="Disney+ Content Background Image"
-            src={bgSmall}
-            priority={true}
-            layout="responsive"
-            objectFit="contain"
-          />
-        </div>
-        <div className="absolute -z-10 hidden h-full w-full md:block">
-          <Image
-            alt="Disney+ Content Background Image"
-            src={bgLarge}
-            priority={true}
-            layout="responsive"
-            objectFit="contain"
-          />
-        </div>
-        <section className="m-auto grid min-h-[70vh]  w-screen max-w-[90%] place-content-center gap-4">
-          <div className="relative -z-10 h-20 w-36 justify-self-center">
-            <Image
-              alt="Disney+ Brand Logo"
-              src="/assets/images/logo.svg"
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
-          <h1 className="text-center text-xl font-semibold">
-            Historias que esperas + historias que no imaginas
-          </h1>
-          <h3 className="text-center text-2xl font-semibold sm:text-2xl">
-            8,99 €<span className="text-xs"> / mes</span>
-          </h3>
-          <a className="cta m-auto w-4/5 px-6 py-3 text-sm sm:text-base">
-            Suscríbete ya
-          </a>
-        </section>
+        <HomeBackground />
+        <HeroSection />
+        <DevicesSection />
+        <Footer />
       </main>
     </>
   );
