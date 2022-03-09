856a22709084a12fd99592733a2e78172157c9b9
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-03-09 21:15:50 +0100

    fix: :bug: Fix z-index for header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
   return (
     <header>
       <nav
-        className={`navbar fixed flex w-full items-center ${
+        className={`navbar fixed z-10 flex w-full items-center ${
           scrolled ? 'bg-primary' : 'bg-transparent'
         }`}
       >
