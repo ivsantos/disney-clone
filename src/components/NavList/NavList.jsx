fbccec11868281d5e5b0fc0c590e957f1672e26a
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-09-10 02:56:59 +0200

    style: :lipstick: Add a bottom bar decorator for nav list items

--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -10,13 +10,13 @@ const NavList = () => {
   return (
     <ul className="ml-8 hidden md:flex md:w-full md:justify-start md:gap-2 md:text-xs">
       {navList.map((item) => (
-        <li key={item.name}>
+        <li key={item.name} className="group">
           <Link href={item.href}>
             <a className="flex items-center space-x-2 p-4">
               <span>
                 <IconWrap Icon={item.icon} size={20} color="white" />
               </span>
-              <span className="hidden lg:block">{item.name}</span>
+              <span className="nav-name">{item.name}</span>
             </a>
           </Link>
         </li>
