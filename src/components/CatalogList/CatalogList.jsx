340d8dd44bc9fa33be17754f8a9f93f5c7ea3155
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-09-10 02:59:57 +0200

    refactor: :recycle: Remove swiper from catalog list

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,72 +1,45 @@
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/effect-coverflow';
-
-import { Navigation, EffectCoverflow } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Image from 'next/image';
 import { IMG_URL } from '@/lib/constants';
+import Image from 'next/image';
+import Link from 'next/link';
 
+/**
+ * Renders a horizontal scrollable list of catalog items.
+ * @typedef {Object} Props
+ * @property {any} list
+ * @param {Props} props
+ * @returns {JSX.Element | null}
+ */
 export default function CatalogList({ list }) {
   if (!list) {
     return null;
   }
 
   console.log(list);
+
   return (
-    <section className="slider mt-0 p-4">
+    <section className="slider mt-0">
       {list.map((category) => (
         <div key={category.title} className="mb-6 w-full">
-          <h2 className="pb-2">{category.title}</h2>
-          <Swiper
-            effect="coverflow"
-            coverflowEffect={{
-              rotate: 0,
-              stretch: 0,
-              depth: 0,
-              modifier: 1,
-              slideShadows: true,
-            }}
-            className="catalog"
-            lazy={{ checkInView: true }}
-            spaceBetween={10}
-            slidesPerView={2.3}
-            breakpoints={{
-              480: {
-                slidesPerView: 3.3,
-              },
-              640: {
-                slidesPerView: 4.3,
-              },
-              768: {
-                slidesPerView: 5.3,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 6.3,
-              },
-              1440: {
-                slidesPerView: 7.3,
-                spaceBetween: 30,
-              },
-            }}
-            navigation={{ disabledClass: '!hidden' }}
-            scrollbar={{ draggable: true, snapOnRelease: true }}
-            modules={[Navigation, EffectCoverflow]}
-          >
+          <h2 className="pl-8">{category.title}</h2>
+          <div className="catalog">
             {category.results.map((item) => (
-              <SwiperSlide key={item.id} className="poster">
-                <Image
-                  className="slider-img"
-                  alt={item.title}
-                  src={`${IMG_URL}${item.backdrop_path}`}
-                  layout="fill"
-                  objectFit="cover"
-                  quality={60}
-                />
-              </SwiperSlide>
+              <div key={item.id}>
+                <Link href={`/${category.type}/${encodeURIComponent(item.id)}`}>
+                  <a className="poster">
+                    <Image
+                      className="catalog-img"
+                      alt={item.title}
+                      src={`${IMG_URL}${
+                        item.backdrop_path || item.poster_path
+                      }`}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </a>
+                </Link>
+              </div>
             ))}
-          </Swiper>
+          </div>
         </div>
       ))}
     </section>
