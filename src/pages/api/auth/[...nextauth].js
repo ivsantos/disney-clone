71443c9ea7307ee9c48d014aed2ac4154b3d87bd
Ivan Santos <18705658+ivsantos@users.noreply.github.com>
2022-09-11 18:50:06 +0200

    feat: :sparkles: Add google as provider for login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,17 @@
 import NextAuth from 'next-auth';
 import CognitoProvider from 'next-auth/providers/cognito';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions = {
   theme: {
     colorScheme: 'dark',
   },
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
@@ -18,6 +23,7 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  site: process.env.NEXTAUTH_URL,
 };
 
 export default NextAuth(authOptions);
